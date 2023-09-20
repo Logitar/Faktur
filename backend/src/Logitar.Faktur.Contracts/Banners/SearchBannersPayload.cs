d7cc292076968014581b017d6ca1f7fcd1f6a42b
Francis Pion <francispion@hotmail.com>
2023-09-20 10:11:00 -0400

    Implemented Banner contracts.

---
+++ backend/src/Logitar.Faktur.Contracts/Banners/SearchBannersPayload.cs
@@ -0,0 +1,8 @@
+﻿using Logitar.Faktur.Contracts.Search;
+
+namespace Logitar.Faktur.Contracts.Banners;
+
+public record SearchBannersPayload : SearchPayload
+{
+  public new IEnumerable<BannerSortOption> Sort { get; set; } = Enumerable.Empty<BannerSortOption>();
+}
---
+++ backend/src/Logitar.Faktur.Contracts/Search/SearchPayload.cs
@@ -0,0 +1,12 @@
+﻿namespace Logitar.Faktur.Contracts.Search;
+
+public record SearchPayload
+{
+  public IEnumerable<Guid> IdIn { get; set; } = Enumerable.Empty<Guid>();
+  public TextSearch Search { get; set; } = new();
+
+  public IEnumerable<SortOption> Sort { get; set; } = Enumerable.Empty<SortOption>();
+
+  public int Skip { get; set; }
+  public int Limit { get; set; }
+}
