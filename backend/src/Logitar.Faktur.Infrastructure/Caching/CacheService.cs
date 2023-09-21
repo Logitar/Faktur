f4a71ff4b231027fd1a6cd476afd7da0466fa09a
Francis Pion <francispion@hotmail.com>
2023-09-21 11:21:56 -0400

    Code Review.

--- backend/src/Logitar.Faktur.Infrastructure/Caching/CacheService.cs
+++ backend/src/Logitar.Faktur.Infrastructure/Caching/CacheService.cs
@@ -16,11 +16,11 @@ internal class CacheService : ICacheService
 
   public Actor? GetActor(ActorId id) => GetItem<Actor>(GetActorKey(id));
   public void RemoveActor(ActorId id) => RemoveItem(GetActorKey(id));
-  public void SetActor(Actor actor) => SetItem(GetActorKey(actor.Id), actor);
+  public void SetActor(Actor actor) => SetItem(GetActorKey(actor.Id), actor, TimeSpan.FromHours(1));
   private static string GetActorKey(string id) => GetActorKey(new ActorId(id));
   private static string GetActorKey(ActorId id) => $"Actor:{id}";
 
   private T? GetItem<T>(string key) => _cache.TryGetValue(key, out object? value) ? (T?)value : default;
   private void RemoveItem(string key) => _cache.Remove(key);
-  private void SetItem<T>(string key, T value) => _cache.Set(key, value);
+  private void SetItem<T>(string key, T value, TimeSpan duration) => _cache.Set(key, value, duration);
 }
