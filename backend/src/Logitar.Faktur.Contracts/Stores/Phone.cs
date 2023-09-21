2ba6bc133668b1b0c53ccbd2b4dd31f9158caf7c
Francis Pion <francispion@hotmail.com>
2023-09-21 11:42:15 -0400

    Implemented Store contracts.

---
+++ backend/src/Logitar.Faktur.Contracts/Stores/Phone.cs
@@ -0,0 +1,8 @@
+﻿namespace Logitar.Faktur.Contracts.Stores;
+
+public record Phone
+{
+  public string? CountryCode { get; set; }
+  public string Number { get; set; } = string.Empty;
+  public string E164Formatted { get; set; } = string.Empty;
+}
---
+++ backend/src/Logitar.Faktur.Contracts/Stores/PhonePayload.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Faktur.Contracts.Stores;
+
+public record PhonePayload
+{
+  public string? CountryCode { get; set; }
+  public string Number { get; set; } = string.Empty;
+}
