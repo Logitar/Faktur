e4f564d03e9d66838e6d9d5c35c7c05b518d62da
Francis Pion <francispion@hotmail.com>
2023-09-21 23:22:00 -0400

    Implemented Department consumers.

---
+++ backend/src/Logitar.Faktur.Infrastructure/Handlers/IDepartmentEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Logitar.Faktur.Domain.Departments.Events;
+
+namespace Logitar.Faktur.Infrastructure.Handlers;
+
+public interface IDepartmentEventHandler
+{
+  Task<bool> HandleAsync(DepartmentRemovedEvent @event, CancellationToken cancellationToken = default);
+  Task<bool> HandleAsync(DepartmentSavedEvent @event, CancellationToken cancellationToken = default);
+}
